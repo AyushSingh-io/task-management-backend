4d916c33b91932d31177163a3471ac37fb5d11cf
AyushSingh-io <ayushsingh.tech01@gmail.com>
2026-08-30 13:20:41 +0530

    correct normalized email and username login query

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -82,7 +82,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const normalizedUsername = username?.toLowerCase()
 
     const user = await User.findOne({
-        $or: [{ normalizedEmail }, { normalizedUsername }]
+        $or: [{ email : normalizedEmail }, { username :normalizedUsername }]
     }).select("+password")
 
     if (!user) {
